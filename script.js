589fd30a1aa07fb77b094700634554104378e9ad
Antonio Savage <afmsavage@gmail.com>
2019-04-28 02:57:45 -0500

    test

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ var rendered = Mustache.render(template, {pokeArray});
 $('#pokeBody').html(rendered);
 }
 
+function renderCategories() {
+  pokeArray = pokeArray.sort(function(a,b){return b.votes-a.votes;});
+  var templateCategory = $('#templateCategory').html();
+  Mustache.parse(templateCategory);
+  var rendered = Mustache.render(templateCategory, {pokeArray});
+  $('#pokeCategory').html(rendered);
+  }
+
 async function callStatic(func, args, types) {
   const calledGet = await client.contractCallStatic(contractAddress, 'sophia-address', func, {args}).catch(e => console.error(e));
   const decodedGet = await client.contractDecodeData(types, calledGet.result.returnValue).catch(e => console.error(e));
@@ -45,6 +53,7 @@ window.addEventListener('load', async () => {
     });
   }
   renderPokes();
+  renderCategories();
 
   $("#loader").hide();
 });
@@ -61,6 +70,7 @@ const foundIndex = pokeArray.findIndex(poke => poke.index == dataIndex);
 pokeArray[foundIndex].votes += parseInt(value, 10);
 
 renderPokes();
+renderCategories();
 
 $("#loader").hide();
 
@@ -85,6 +95,50 @@ $('#registerBtn').click(async function(){
 });
 
 renderPokes();
+renderCategories();
 
 $("#loader").hide();
 });
+// filter by category
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsById("pokeCategory");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveId(x[i], "show");
+    if (x[i].id.indexOf(c) > -1) w3AddId(x[i], "show");
+  }
+}
+
+function w3AddId(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.id.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.id += " " + arr2[i];}
+  }
+}
+
+function w3RemoveId(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.id.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.id = arr1.join(" ");
+}
+
+// Add active id to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsById("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsById("active");
+    current[0].id = current[0].id.replace(" active", "");
+    this.id += " active";
+  });
+}
